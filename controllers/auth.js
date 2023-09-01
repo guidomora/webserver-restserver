d2bb0d32f84c34e66176b6251a975667e334756d
guido morabito <guidomorabito161@hotmail.com>
2023-09-01 17:53:10 -0300

    Google sign-in

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,4 +47,14 @@ const login = async (req, res = response) => {
   }
 };
 
-module.exports = { login };
+
+const googleSignIn = async (req, res = response) => {
+  const {id_token} = req.body
+
+  res.json({
+    id_token,
+    msg: 'Todo ok'
+  })
+}
+
+module.exports = { login, googleSignIn };
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,26 @@
 const { check } = require("express-validator"); // para hacer validaciones
-const { login } = require("../controllers/auth");
+const { login, googleSignIn } = require("../controllers/auth");
 const { Router } = require("express");
 const { validateFields } = require("../middlewares/validate-fields");
 const router = Router();
 
+router.post(
+  "/login",
+  [
+    check("mail", "Mail is required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
 
-router.post('/login', [
-    check('mail', 'Mail is required').isEmail(),
-    check('password', 'Password is required').not().isEmpty(),
-    validateFields
-], login)
+router.post(
+  "/google",
+  [
+    check("id_token", "id_token is required").not().isEmpty(),
+    validateFields,
+  ],
+  googleSignIn
+);
 
-module.exports = router
+module.exports = router;
