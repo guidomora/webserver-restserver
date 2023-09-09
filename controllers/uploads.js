3cc952449522db90a3fc2da8c4a26c529d13b769
guido morabito <guidomorabito161@hotmail.com>
2023-09-09 20:25:05 -0300

    cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,16 +1,16 @@
-const path = require('path')
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 const { request, response } = require("express");
 const { uploadFileHelper } = require("../helpers/upload-file");
-const User = require('../models/user')
-const Product = require('../models/product')
+const User = require("../models/user");
+const Product = require("../models/product");
 
 const uploadFile = async (req = request, res = response) => {
   // Copiado del repositorio de express-fileupload
 
   try {
     // const name = await uploadFileHelper(req.files, ['txt'], 'textos') //// Modelo de como quedaria con todos los argumentos
-    const name = await uploadFileHelper(req.files, undefined, 'imgs');
+    const name = await uploadFileHelper(req.files, undefined, "imgs");
 
     res.json({
       name,
@@ -23,53 +23,102 @@ const uploadFile = async (req = request, res = response) => {
 };
 
 const updateFile = async (req = request, res = response) => {
-  const {collection, id} =req.params
+  const { collection, id } = req.params;
 
   let model; // se va a almacenar el modelo
 
-  switch (collection) { // validamos que segun la coleccion que recibamos, el id sea correcto
-    case 'users':
-      model = await User.findById(id)
+  switch (
+    collection // validamos que segun la coleccion que recibamos, el id sea correcto
+  ) {
+    case "users":
+      model = await User.findById(id);
       if (!model) {
         return res.status(400).json({
-          msg: 'There is no user with that id'
-        })
+          msg: "There is no user with that id",
+        });
       }
       break;
-      case 'products':
-      model = await Product.findById(id)
+    case "products":
+      model = await Product.findById(id);
       if (!model) {
         return res.status(400).json({
-          msg: 'There is no product with that id'
-        })
+          msg: "There is no product with that id",
+        });
       }
       break;
-  
+
     default:
       return res.status(500).json({
-        msg: 'Forgot to validate this :('
+        msg: "Forgot to validate this :(",
       });
   }
 
   // limpiar imagenes previas: reemplaza la imagen actual por la nueva que se sube
   if (model.img) {
-    const imgPath = path.join(__dirname, '../uploads', collection, model.img)
+    const imgPath = path.join(__dirname, "../uploads", collection, model.img);
     // puede ser que el imgPath no exista entonces vamos a averiguar si existe
-    if (fs.existsSync(imgPath)) { // verifica
-      fs.unlinkSync(imgPath) // lo borra
+    if (fs.existsSync(imgPath)) {
+      // verifica
+      fs.unlinkSync(imgPath); // lo borra
     }
   }
 
   // al ser correcto se crea la imagen, con la carpeta con el nombre de la coleccion
   const name = await uploadFileHelper(req.files, undefined, collection);
-  model.img = name // se sube la imagen al modelo
+  model.img = name; // se sube la imagen al modelo
 
-  await model.save() // se guarda en la db
-  
-  res.json(model)
-}
+  await model.save(); // se guarda en la db
+
+  res.json(model);
+};
+
+const showImg = async (req = request, res = response) => {
+  const { id, collection } = req.params;
+
+  let model; // se va a almacenar el modelo
+
+  switch (
+    collection // validamos que segun la coleccion que recibamos, el id sea correcto
+  ) {
+    case "users":
+      model = await User.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: "There is no user with that id",
+        });
+      }
+      break;
+    case "products":
+      model = await Product.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: "There is no product with that id",
+        });
+      }
+      break;
+
+    default:
+      return res.status(500).json({
+        msg: "Forgot to validate this :(",
+      });
+  }
+
+  // limpiar imagenes previas: reemplaza la imagen actual por la nueva que se sube
+  if (model.img) {
+    const imgPath = path.join(__dirname, "../uploads", collection, model.img);
+    // puede ser que el imgPath no exista entonces vamos a averiguar si existe
+    if (fs.existsSync(imgPath)) {
+     return res.sendFile(imgPath) // para retornar la imagen tenemos que suar sendFile
+    }
+  }
+
+  const noImgPath = path.join(__dirname, "../assets/no-image.jpg") // imagen que retorna cuando no hay imagen en un usuario/producto
+
+  res.sendFile(noImgPath);
+};
 
 module.exports = {
   uploadFile,
-  updateFile
+  updateFile,
+  showImg,
 };
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,20 +1,36 @@
 const { check } = require("express-validator"); // para hacer validaciones
 const { Router } = require("express");
 const { validateFields } = require("../middlewares/validate-fields");
-const { uploadFile, updateFile } = require("../controllers/uploads");
+const { uploadFile, updateFile, showImg } = require("../controllers/uploads");
 const { allowedCollections } = require("../helpers/db-validators");
 const { validateUploadedFile } = require("../middlewares/validate-file");
 const router = Router();
 
-router.post('/',validateUploadedFile, uploadFile) // se puede usar un put pero el estandar es POST
+router.post("/", validateUploadedFile, uploadFile); // se puede usar un put pero el estandar es POST
 
-router.put('/:collection/:id', [
+router.put(
+  "/:collection/:id",
+  [
     validateUploadedFile,
-    check('id',"Not a valid Mongo id").isMongoId(),
+    check("id", "Not a valid Mongo id").isMongoId(),
 
     // la c hace referencia a la coleccion recibida en el put y el array son las opciones permitidas
-    check('collection').custom(c => allowedCollections(c, ['users', 'products'])),
-    validateFields
-], updateFile)
+    check("collection").custom((c) =>
+      allowedCollections(c, ["users", "products"])
+    ),
+    validateFields,
+  ],
+  updateFile
+);
+
+router.get("/:collection/:id", [
+  check("id", "Not a valid Mongo id").isMongoId(),
+
+  // la c hace referencia a la coleccion recibida en el put y el array son las opciones permitidas
+  check("collection").custom((c) =>
+    allowedCollections(c, ["users", "products"])
+  ),
+  validateFields,
+], showImg);
 
 module.exports = router;
